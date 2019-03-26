2d00971f40318307181cda7489b9d621beaf3755
Sean Houde <seanhoude@gmail.com>
2019-03-26 14:40:49 -0400

    Add test files

---
+++ src/com/qoala/DeckTest.java
@@ -0,0 +1,26 @@
+package com.qoala;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deck() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deck1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createDeck() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void shuffleDeck() {
+    }
+}
