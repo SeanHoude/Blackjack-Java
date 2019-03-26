cc12acb9fa1377055d8caebfbd2dd5635a2e5b55
Sean Houde <seanhoude@gmail.com>
2019-03-26 13:41:38 -0400

    Initial commit

---
+++ src/com/qoala/Card.java
@@ -0,0 +1,8 @@
+package com.qoala;
+
+public class Card {
+    public Card(String suit, Rank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
---
+++ src/com/qoala/Deck.java
@@ -0,0 +1,11 @@
+package com.qoala;
+
+public class Deck {
+
+    public deck(int decks) {
+        for (int i = 0; i <= decks; i++) {
+
+        }
+    }
+
+}
